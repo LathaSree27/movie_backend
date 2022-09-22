b8d7af40b931a6bf8b5444cf9a7d1f7679fd858f
Deepa K <deepa.subramanyamthoughtworks.com@Deepas-MacBook-Pro.local>
2022-09-22 16:30:52 +0530

    [Deepa | Vaishnavi Bandaru] Fix. admin password encryption

--- src/main/java/com/booking/DataSeeder.java
+++ src/main/java/com/booking/DataSeeder.java
@@ -1,6 +1,5 @@
 package com.booking;
 
-import com.booking.config.security.PasswordEncoderConfig;
 import com.booking.users.User;
 import com.booking.users.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,20 +11,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class DataSeeder {
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     public DataSeeder() {
     }
-
 
     @Bean
     CommandLineRunner initDatabase(UserRepository repository) {
 
         return args -> {
             if (repository.findByUsername("seed-user-1").isEmpty()) {
-                repository.save(new User("seed-user-1", "foobar", "admin"));
+                repository.save(new User("seed-user-1", passwordEncoder.encode("foobar"), "admin"));
             }
             if (repository.findByUsername("seed-user-2").isEmpty()) {
-                repository.save(new User("seed-user-2", "foobar", "admin"));
+                repository.save(new User("seed-user-2", passwordEncoder.encode("foobar"), "admin"));
             }
         };
     }
